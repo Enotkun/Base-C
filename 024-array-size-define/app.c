928cdde09ad33c0a2b64751c90c5b8c53b7fecdd
Anton <kai070191@gmail.com>
2025-03-06 07:18:07 +0200

    feat: array size and init

---
+++ 024-array-size-define/app.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int main(void)
+{
+    int prices[SIZE] = {100, 200, 300, 400, 500};
+
+    printf("first element: %i\n", prices[0]);
+    printf("prev element: %i\n", prices[SIZE-2]);
+    printf("last element: %i\n", prices[SIZE-1]);
+
+    return 0;
+}
